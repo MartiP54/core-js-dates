32a8702f03bd5ba7aeb71e0dfb626fcec11129e7
MartiP54 <116718918+MartiP54@users.noreply.github.com>
2024-03-25 21:55:54 +0700

    add: add 12 task

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -251,8 +251,9 @@ function getNextFridayThe13th(date) {
  * Date(2024, 5, 1) => 2
  * Date(2024, 10, 10) => 4
  */
-function getQuarter(/* date */) {
-  throw new Error('Not implemented');
+function getQuarter(date) {
+  const dateTemp = new Date(date);
+  return Math.ceil(dateTemp.getMonth() / 3);
 }
 
 /**
