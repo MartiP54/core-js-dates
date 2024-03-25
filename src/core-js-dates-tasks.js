4c70551c4f0171a9b1771452c02bcc50a58a1e68
MartiP54 <116718918+MartiP54@users.noreply.github.com>
2024-03-25 16:48:48 +0700

    feat: add 5,6 task

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -88,8 +88,9 @@ function getNextFriday(date) {
  * 1, 2024 => 31
  * 2, 2024 => 29
  */
-function getCountDaysInMonth(/* month, year */) {
-  throw new Error('Not implemented');
+function getCountDaysInMonth(month, year) {
+  const dateTemp = new Date(year, month, 0);
+  return dateTemp.getDate();
 }
 
 /**
@@ -103,8 +104,10 @@ function getCountDaysInMonth(/* month, year */) {
  * '2024-02-01T00:00:00.000Z', '2024-02-02T00:00:00.000Z'  => 2
  * '2024-02-01T00:00:00.000Z', '2024-02-12T00:00:00.000Z'  => 12
  */
-function getCountDaysOnPeriod(/* dateStart, dateEnd */) {
-  throw new Error('Not implemented');
+function getCountDaysOnPeriod(dateStart, dateEnd) {
+  const start = Date.parse(dateStart);
+  const end = Date.parse(dateEnd);
+  return (end - start) / (1000 * 60 * 60 * 24) + 1;
 }
 
 /**
